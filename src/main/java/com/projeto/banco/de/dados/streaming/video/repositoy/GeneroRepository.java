a4307d1f619f76b93de1d5c0297202a56b1ff3b0
Tibuh <vitortiburtino10@gmail.com>
2021-07-24 01:43:38 -0300

    Criacao CRUD Filme

--- src/main/java/com/projeto/banco/de/dados/streaming/video/repositoy/GeneroRepository.java
+++ src/main/java/com/projeto/banco/de/dados/streaming/video/repositoy/GeneroRepository.java
@@ -1,9 +1,12 @@
 package com.projeto.banco.de.dados.streaming.video.repositoy;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.banco.de.dados.streaming.video.entity.Genero;
 
 public interface GeneroRepository extends JpaRepository<Genero, Long> {
 
+	Optional<Genero> findByGenero(String genero);
 }
